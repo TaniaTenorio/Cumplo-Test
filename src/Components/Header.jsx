11edefb3b2f19ca805115e13ad1fbaf66b2935b5
TaniaTenorio <taniatenorio@hotmail.com>
2019-12-20 16:21:18 -0600

    add(Header.jsx): creating Header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <h1 className="main-title green-background align-center">My Banking System</h1>
+    )
+};
+
+export default Header;
