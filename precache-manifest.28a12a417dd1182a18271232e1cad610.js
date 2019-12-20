031584ff649757cad79ffbef13935c0d63a0d29d
TaniaTenorio <taniatenorio@hotmail.com>
2019-12-19 19:35:58 -0600

    Updates

---
+++ precache-manifest.28a12a417dd1182a18271232e1cad610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89cb9da4ae297e54f8a8a4ea60f4d5b9",
+    "url": "/Cumplo-Test/index.html"
+  },
+  {
+    "revision": "388dd6903892360c02cd",
+    "url": "/Cumplo-Test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b91719ecfbb82fefa957",
+    "url": "/Cumplo-Test/static/js/2.d65a0115.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Cumplo-Test/static/js/2.d65a0115.chunk.js.LICENSE"
+  },
+  {
+    "revision": "388dd6903892360c02cd",
+    "url": "/Cumplo-Test/static/js/main.12956fc1.chunk.js"
+  },
+  {
+    "revision": "750f2a0bad04959a12fe",
+    "url": "/Cumplo-Test/static/js/runtime-main.8d33c289.js"
+  }
+]);
