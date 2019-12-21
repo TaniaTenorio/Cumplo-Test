e691ffea1b8a29ade911a0cdd1066acd057312cf
TaniaTenorio <taniatenorio@hotmail.com>
2019-12-21 00:44:19 -0600

    feat(App.jsx): adding new route for Quinn Wiley branch

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BanksContainer from './Views/BanksContainer';
 import BranchesContainer from './Views/BranchesContainer';
 import EmployeesContainer from './Views/EmployeesContainer';
+import QuinnWileyContainer from './Views/QuinnWileyContainer';
 
 import './App.css';
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <Route exact path="/" component={BanksContainer} />
-      <Route path="/branches" component={BranchesContainer} />
+      <Route exact path="/" component={BranchesContainer} />
+      <Route path="/banks" component={BanksContainer} />
       <Route path="/employees" component={EmployeesContainer} />
+      <Route path="/Quinn Wiley" component={QuinnWileyContainer} />
     </Router>
   );
 }
