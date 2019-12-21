ebf8981f9dbcd0841d9a7e31f6d31c48b0cc983b
TaniaTenorio <taniatenorio@hotmail.com>
2019-12-21 00:51:42 -0600

    creating component for section title

---
+++ src/Components/SectionTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SectionTitle(props) {
+    return(
+        <div className="section-title">
+            <h2>{props.section}</h2>
+        </div>
+    )
+}
+
+export default SectionTitle;
