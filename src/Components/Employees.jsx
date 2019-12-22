0c8def48e421bd1b3a8ff62eea75c3c0ab8f21d0
TaniaTenorio <taniatenorio@hotmail.com>
2019-12-21 18:13:49 -0600

    fix(Employees): deleting title

--- src/Components/Employees.jsx
+++ src/Components/Employees.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 function Employees(props) {
     return(
         <div>
-            <p>Name: {props.name} {props.middleName} {props.lastName}</p>
-            <p>Branch:</p>
+            <p>{props.name} {props.middleName} {props.lastName}</p>
         </div>
     )
 };
