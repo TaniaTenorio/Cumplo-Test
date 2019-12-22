d140ed61b0000f7a914d241140c12feb87e9e58e
TaniaTenorio <taniatenorio@hotmail.com>
2019-12-21 18:16:20 -0600

    trying to fetch next pages from api

--- src/Views/QuinnWileyContainer.jsx
+++ src/Views/QuinnWileyContainer.jsx
@@ -2,24 +2,41 @@ import React, { Component } from 'react';
 
 import Header from '../Components/Header';
 import SectionTitle from '../Components/SectionTitle';
-import EmployeesContainer from './EmployeesContainer';
+import Employees from '../Components/Employees';
+import EmployeeForm from '../Components/EmployeeForm';
 
 export default class QuinnWileyContainer extends Component  {
     constructor(props) {
         super(props)
         this.state = {
-
+            employees: [],
         }
     }
+
+    componentDidMount() {
+        fetch("https://tryouts-cumplo.herokuapp.com/employees/")
+            .then(response => response.json())
+            .then(employeesJson => this.setState({employees: employeesJson.results}))
+    };
+    
     render() {
+        // if(this.state.isFetched) {
+        //     return 'Loading...'
+        // }
+        console.log(this.state.employees)
+
         return(
-            <section>
+            <div>
                 <Header />
                 <SectionTitle section="Quinn Wiley" />
-                <EmployeesContainer />
-            </section>
+            {this.state.employees.map((employee) => {
+                if(employee.branch ===1){
+                    const employeeTemplate = <Employees name={employee.name} middleName={employee.middle_name} lastName={employee.last_name}key={employee.id}/>
+                    return employeeTemplate
+                }
+            })}
+             <EmployeeForm />
+            </div>
         )
     }
-    
-}
-
+};
