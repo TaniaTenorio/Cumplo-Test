2249474316c5587e5e21e236f801214ab6e56cb0
TaniaTenorio <taniatenorio@hotmail.com>
2019-12-21 18:11:10 -0600

    add(App.jsx): adding route for Hedley Todd branch

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import BanksContainer from './Views/BanksContainer';
 import BranchesContainer from './Views/BranchesContainer';
 import EmployeesContainer from './Views/EmployeesContainer';
 import QuinnWileyContainer from './Views/QuinnWileyContainer';
+import HedleyToddContainer from './Views/HedleyToddContainer';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
       <Route path="/banks" component={BanksContainer} />
       <Route path="/employees" component={EmployeesContainer} />
       <Route path="/Quinn Wiley" component={QuinnWileyContainer} />
+      <Route path="/Hedley Todd" component={HedleyToddContainer} />
     </Router>
   );
 }
